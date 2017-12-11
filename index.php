c9fe7d198fa85085b53fc503852a344e8de9f0a1
Seksan <microsekmcu@gmail.com>
2017-12-11 22:18:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require("phpMQTT.php");
 
 $server = "m11.cloudmqtt.com";     // change if necessary
-$port = 34434;                     // change if necessary
+$port = 14434;                     // change if necessary
 $username = "test";                   // set your username
 $password = "12345";                   // set your password
 $client_id = "phpMQTT-publisher"; // make sure this is unique for connecting to sever - you could use uniqid()
