b0c929a8db6080cc5bc478bb0b5eb85a5dda172b
Seksan <microsekmcu@gmail.com>
2017-12-12 14:57:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,8 @@ $port = 14434;                     // change if necessary
 $username = "test";                   // set your username
 $password = 12345;                   // set your password
 $client_id = ""; // make sure this is unique for connecting to sever - you could use uniqid()
-
-//$mqtt = new bluerhinos\phpMQTT($host, $port, "".rand());
-
-$mqtt = new bluerhinos\phpMQTT($server, $port, "".rand());
+//$mqtt = new bluerhinos\phpMQTT($server, $port, "".rand());
+$mqtt = new bluerhinos\phpMQTT($server, $port, $client_id);
 if ($mqtt->connect(true, NULL, $username, $password)) {
 	$mqtt->publish("/ESP/LED", "PHP_HEROKU", 0);
 	$mqtt->close();
