4bf06d02fd043abc3478d53e5aed394ef7d23806
Seksan <microsekmcu@gmail.com>
2017-12-12 09:38:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,13 @@ require("phpMQTT.php");
 $server = "m11.cloudmqtt.com";     // change if necessary
 $port = 24434;                     // change if necessary
 $username = "test";                   // set your username
-$password = "12345";                   // set your password
+$password = 12345;                   // set your password
 $client_id = "phpMQTT-publisher"; // make sure this is unique for connecting to sever - you could use uniqid()
 
 $mqtt = new phpMQTT($server, $port, $client_id);
 
 if ($mqtt->connect(true, NULL, $username, $password)) {
-	$mqtt->publish("/ESP/LED", "LEDON", 0);
+	$mqtt->publish("/ESP/LED", "PHP_HEROKU", 0);
 	$mqtt->close();
 } else {
     echo "Time out!\n";
