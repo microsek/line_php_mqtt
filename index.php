15487411dbd6862e96390f309d7cdee35a5170cf
Seksan <microsekmcu@gmail.com>
2017-12-12 14:28:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 require("phpMQTT.php");
 
-$server   = "***.cloudmqtt.com"; 
-$port     = ;
-$username = "";
-$password = "";
+$server = "m11.cloudmqtt.com";     // change if necessary
+$port = 14434;                     // change if necessary
+$username = "test";                   // set your username
+$password = "12345";                   // set your password
+$client_id = "phpMQTT-subscriber"; // make sure this is unique for connecting to sever - you could use uniqid()
 
 $message = "Hello CloudAMQP MQTT!";
 //MQTT client id to use for the device. "" will generate a client id automatically
