d1c5a31800507954a626c83bb1b06d8c5ad27bec
Seksan <microsekmcu@gmail.com>
2017-12-12 14:54:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require("phpMQTT.php");
 $server = "m11.cloudmqtt.com";     // change if necessary
 $port = 14434;                     // change if necessary
 $username = "test";                   // set your username
-$password = "12345";                   // set your password
+$password = 12345;                   // set your password
 $client_id = ""; // make sure this is unique for connecting to sever - you could use uniqid()
 
 $mqtt = new bluerhinos\phpMQTT($host, $port, "".rand());
