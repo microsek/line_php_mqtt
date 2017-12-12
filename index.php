2902e39169c8c12f71559a6f84d6e1d885555358
Seksan <microsekmcu@gmail.com>
2017-12-12 14:41:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,19 +4,17 @@ require("phpMQTT.php");
 $server = "m11.cloudmqtt.com";     // change if necessary
 $port = 14434;                     // change if necessary
 $username = "test";                   // set your username
-$password = "12345";                   // set your password
-$client_id = "phpMQTT-subscriber"; // make sure this is unique for connecting to sever - you could use uniqid()
+$password = 12345;                   // set your password
+$client_id = "phpMQTT-publisher"; // make sure this is unique for connecting to sever - you could use uniqid()
 
-$message = "Hello CloudAMQP MQTT!";
-//MQTT client id to use for the device. "" will generate a client id automatically
-//$mqtt = new bluerhinos\phpMQTT($host, $port, "".rand());
 $mqtt = new bluerhinos\phpMQTT($host, $port, $client_id);
 
-if ($mqtt->connect(true,NULL,$username,$password)) {
-  $mqtt->publish("/ESP/LED",$message, 0);
-  $mqtt->close();
-}else{
-  echo "Fail or time out";
+
+if ($mqtt->connect(true, NULL, $username, $password)) {
+	$mqtt->publish("/ESP/LED", "PHP_HEROKU", 0);
+	$mqtt->close();
+} else {
+    echo "Time out!\n";
 }
 
 ?>
