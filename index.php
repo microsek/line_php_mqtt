26924c6645f9564be6736524622c7922fcc8426d
Seksan <microsekmcu@gmail.com>
2017-12-12 14:58:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,14 @@ $server = "m11.cloudmqtt.com";     // change if necessary
 $port = 14434;                     // change if necessary
 $username = "test";                   // set your username
 $password = 12345;                   // set your password
-$client_id = ""; // make sure this is unique for connecting to sever - you could use uniqid()
-//$mqtt = new bluerhinos\phpMQTT($server, $port, "".rand());
-$mqtt = new bluerhinos\phpMQTT($server, $port, $client_id);
+$client_id = "Microsek"; // make sure this is unique for connecting to sever - you could use uniqid()
+
+$mqtt = new bluerhinos\phpMQTT($server, $port, "".rand());
+
 if ($mqtt->connect(true, NULL, $username, $password)) {
 	$mqtt->publish("/ESP/LED", "PHP_HEROKU", 0);
 	$mqtt->close();
+	echo "Finished Publish\n";
 } else {
     echo "Time out!\n";
 }
