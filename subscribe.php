41229cd4cb3f2f2e01f7924a52c7bf8df0e07cc8
Seksan <microsekmcu@gmail.com>
2017-12-13 20:55:46 +0700

    Update subscribe.php

--- subscribe.php
+++ subscribe.php
@@ -1,32 +1,25 @@
 <?php
 
-require("../phpMQTT.php");
+require("phpMQTT.php");
+require("config.php");
+$mqtt = new bluerhinos\phpMQTT($host, $port, "ClientID".rand());
 
-
-$server = "m11.cloudmqtt.com";     // change if necessary
-$port = 14434;                     // change if necessary
-$username = "test";                   // set your username
-$password = "12345";                   // set your password
-$client_id = "phpMQTT-subscriber"; // make sure this is unique for connecting to sever - you could use uniqid()
-
-$mqtt = new phpMQTT($server, $port, $client_id);
-
-if(!$mqtt->connect(true, NULL, $username, $password)) {
-	exit(1);
+if(!$mqtt->connect(true,NULL,$username,$password)){
+  exit(1);
 }
 
-$topics['/ESP/LED'] = array("qos" => 0, "function" => "procmsg");
-$mqtt->subscribe($topics, 0);
+//currently subscribed topics
+$topics['/microsek/esp'] = array("qos"=>0, "function"=>"procmsg");
+$mqtt->subscribe($topics,0);
 
 while($mqtt->proc()){
-		
 }
-
 
 $mqtt->close();
-
-function procmsg($topic, $msg){
-		echo "Msg Recieved: " . date("r") . "\n";
-		echo "Topic: {$topic}\n\n";
-		echo "\t$msg\n\n";
+function procmsg($topic,$msg){
+  echo "Msg Recieved: $msg";
 }
+
+?>
+
+
