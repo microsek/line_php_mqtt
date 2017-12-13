79fdd29f9b4a231249369f0f80bda959155f2a8b
Seksan <microsekmcu@gmail.com>
2017-12-13 20:49:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -60,24 +60,9 @@ if (!is_null($events['events'])) {
             
             elseif($textin_cmd[0]=='สถานะ')
             {
-                $mqtt = new bluerhinos\phpMQTT($server, $port, "".rand());
-
-                if(!$mqtt->connect(true,NULL,$username,$password)){
-                    exit(1);
-                }
-
-                //currently subscribed topics
-                $topics['/microsek/esp'] = array("qos"=>0, "function"=>"procmsg");
-                $mqtt->subscribe($topics,0);
-
-                while($mqtt->proc()){
-                }
-
-                $mqtt->close();
-                function procmsg($topic,$msg){
-                    $messages = [
-                    'type' => 'text',
-                    'text' => $msg
+                $messages = [
+                        'type' => 'text',
+                        'text' => "ติดต่อ mqtt ไม่ได้"
                     ];
 
             }
